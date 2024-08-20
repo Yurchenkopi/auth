4ed352b253517d1ad057d073450e78bff16aeef1
Pavel <heppie@yandex.ru>
2024-08-20 22:43:45 +0300

    5. Обработка исключений и Spring REST [#504797]

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -1,13 +1,23 @@
 package ru.job4j.auth.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.auth.model.Person;
 import ru.job4j.auth.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @RestController
 @AllArgsConstructor
@@ -17,6 +27,10 @@ public class PersonController {
 
     private PasswordEncoder encoder;
 
+    private final ObjectMapper objectMapper;
+
+    private static final Logger LOG = LoggerFactory.getLogger(PersonController.class.getName());
+
     @GetMapping("/")
     public List<Person> findAll() {
         return simplePersonService.findAll();
@@ -24,13 +38,24 @@ public class PersonController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Person> findById(@PathVariable int id) {
+        if (id == 0) {
+            throw new IllegalArgumentException("User id must not be equal to zero.");
+        }
         return simplePersonService.findById(id)
                 .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "User is not found. Please, check id."
+                ));
     }
 
     @PostMapping({"/", "/sign-up"})
     public ResponseEntity<Person> create(@RequestBody Person person) {
+        if (person.getLogin() == null || person.getPassword() == null) {
+            throw new NullPointerException("Username and password mustn't be empty");
+        }
+        if (!passwordIsValid(person.getPassword())) {
+            throw new IllegalArgumentException("Password must contain at least one lowercase and at least one uppercase letter.");
+        }
         person.setPassword(encoder.encode(person.getPassword()));
         return simplePersonService.save(person)
                 .map(ResponseEntity::ok)
@@ -39,6 +64,15 @@ public class PersonController {
 
     @PutMapping("/")
     public ResponseEntity<Void> update(@RequestBody Person person) {
+        if (person.getId() == 0) {
+            throw new IllegalArgumentException("User id must not be equal to zero.");
+        }
+        if (person.getLogin() == null || person.getPassword() == null) {
+            throw new NullPointerException("Username and password mustn't be empty");
+        }
+        if (!passwordIsValid(person.getPassword())) {
+            throw new IllegalArgumentException("Password must contain at least one lowercase and at least one uppercase letter.");
+        }
         ResponseEntity<Void> status = ResponseEntity.notFound().build();
         boolean isUpdated = simplePersonService.update(person);
         if (isUpdated) {
@@ -49,6 +83,9 @@ public class PersonController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable int id) {
+        if (id == 0) {
+            throw new IllegalArgumentException("User id must not be equal to zero.");
+        }
         ResponseEntity<Void> status = ResponseEntity.notFound().build();
         Person person = new Person();
         person.setId(id);
@@ -57,6 +94,23 @@ public class PersonController {
             status = ResponseEntity.ok().build();
         }
         return status;
+    }
+
+    @ExceptionHandler(value = { IllegalArgumentException.class })
+    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
+            put("message", e.getMessage());
+            put("type", e.getClass());
+        }}));
+        LOG.error(e.getLocalizedMessage());
+    }
+
+    private boolean passwordIsValid(String password) {
+        String regex = "^(?=.*[a-z])(?=.*[A-Z]).+$";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(password).find();
     }
 
 }
