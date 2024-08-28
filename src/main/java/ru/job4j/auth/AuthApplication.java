e0836414ef2c381f8146f7a3ecb3dabb61b2ce37
Yurchenkopi <heppie@yandex.ru>
2024-08-28 15:04:06 +0300

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/auth/AuthApplication.java
+++ src/main/java/ru/job4j/auth/AuthApplication.java
@@ -1,20 +1,38 @@
 package ru.job4j.auth;
 
+import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
-public class AuthApplication {
+import javax.sql.DataSource;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
+@SpringBootApplication
+public class AuthApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AuthApplication.class);
+	}
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
 	}
 }
